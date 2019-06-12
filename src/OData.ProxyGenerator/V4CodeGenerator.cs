7e9a1efd0a809a35156730f4b74cd382adc3e3dc
Erwin Werkman <werkman.erwin@gmail.com>
2019-06-12 16:54:24 +0200

    Initial version

---
+++ src/OData.ProxyGenerator/V4CodeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using OData.ProxyGenerator.Templates;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OData.ProxyGenerator
+{
+    public class V4CodeGenerator
+    {
+        public string GenerateCode(string namespacePrefix, 
+            string metadataDocumentUri, 
+            bool useDataServiceCollection = true, 
+            bool ignoreUnexpectedElementsAndAttributes = false, 
+            bool enableNamingAlias = false)
+        {
+            ODataT4CodeGenerator t4CodeGenerator = new ODataT4CodeGenerator
+            {
+                NamespacePrefix = namespacePrefix,
+                MetadataDocumentUri = metadataDocumentUri,
+                UseDataServiceCollection = useDataServiceCollection,
+                TargetLanguage = ODataT4CodeGenerator.LanguageOption.CSharp,
+                IgnoreUnexpectedElementsAndAttributes = ignoreUnexpectedElementsAndAttributes,
+                EnableNamingAlias = enableNamingAlias
+            };
+
+            return t4CodeGenerator.TransformText();
+        }
+    }
+}
---
+++ src/ODataConnectedServiceConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using OData.ProxyGenerator;
+
+namespace ODataConnectedServiceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var codeGenerator = new V4CodeGenerator();
+            var code = codeGenerator.GenerateCode(null, "https://localhost:5000/api/$metadata");
+        }      
+
+    }
+}
