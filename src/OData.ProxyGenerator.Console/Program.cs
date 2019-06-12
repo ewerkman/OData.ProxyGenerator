7ce0f66fe1fa7f72e2dd8ffca19c7973ef7b5fcd
Erwin Werkman <werkman.erwin@gmail.com>
2019-06-12 20:43:20 +0200

    Added console app

---
+++ src/OData.ProxyGenerator.Console/Program.cs
@@ -0,0 +1,90 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using OData.ProxyGenerator;
+using System;
+using System.IO;
+
+namespace ODataConnectedServiceConsole
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('n', "namespacePrefix", Required = false, HelpText = "The namespace of the client code generated. It replaces the original namespace in the metadata document, unless the model has several namespaces.")]
+            public string NamespacePrefix { get; set; }
+
+            [Option('m', "metadataDocumentUri", Required = true, HelpText = "The URI of the metadata document. The value must be set to a valid service document URI or a local file path " +
+                                                            "eg : \"http://services.odata.org/V4/OData/OData.svc/\", \"File:///C:/Odata.edmx\", or @\"C:\\Odata.edmx\".  " +
+                                                            "NOTE: If the OData service requires authentication for accessing the metadata document, the value of" +
+                                                            "MetadataDocumentUri has to be set to a local file path, or the client code generation process will fail.")]
+            public string MetadataDocumentUri { get; set; }
+
+            [Option('o', "outputFile", Required = false, HelpText = "Filename of file to output to. If no outputfile is configured, the code will be output to the console.")]
+            public string OutputFile { get; set; }
+
+            [Option("overwrite", Required = false, Default = false, HelpText = "Overwrites the output file if it already exists.")]
+            public bool Overwrite { get; set; }
+
+            [Option('u', "useDataServiceCollection", Required = false, Default = true, HelpText = "The use of DataServiceCollection enables entity and property tracking. The value must be set to true or false.")]
+            public bool UseDataServiceCollection { get; set; }
+
+            [Option('i', "ignoreUnexpectedElementsAndAttributes", Required = false, Default = false, HelpText = "This flag indicates whether to ignore unexpected elements and attributes in the metadata document and generate the client code if any. The value must be set to true or false.")]
+            public bool IgnoreUnexpectedElementsAndAttributes { get; set; }
+
+            [Option('e', "enableNamingAlias ", Required = false, Default = false, HelpText = "This flag indicates whether to enable naming alias. The value must be set to true or false.")]
+            public bool EnableNamingAlias { get; internal set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    var codeGenerator = new V4CodeGenerator();
+                    var generatedCode = codeGenerator.GenerateCode(o.NamespacePrefix,
+                                 o.MetadataDocumentUri,
+                                 o.UseDataServiceCollection,
+                                 o.IgnoreUnexpectedElementsAndAttributes,
+                                 o.EnableNamingAlias);
+
+                    if (string.IsNullOrEmpty(o.OutputFile))
+                    {
+                        Console.Write(generatedCode);
+                    }
+                    else
+                    {
+                        FileInfo fileInfo = new FileInfo(o.OutputFile);
+
+                        if(string.IsNullOrEmpty(fileInfo.Name) || fileInfo.Extension != "cs")
+                        {
+                            Console.Write($"'{o.OutputFile}' is not a valid filename.");
+                        }
+                        else if (fileInfo.Exists && !o.Overwrite)
+                        {
+                            Console.Write($"'{o.OutputFile}' already exists. Use --overwrite to overwrite the existing file.");
+                        }
+                        else
+                        {
+                            Console.Write($"Writing generated code to '{o.OutputFile}'");
+
+                            WriteGeneratedCodeToFile(generatedCode, fileInfo);
+                        }
+                    }
+
+                    Console.WriteLine("Finished!");
+                });
+        }
+
+        private static void WriteGeneratedCodeToFile(string generatedCode, FileInfo toFileInfo)
+        {
+            Directory.CreateDirectory(toFileInfo.Directory.FullName);
+            toFileInfo.Delete();
+
+            using (StreamWriter writer = File.CreateText(toFileInfo.FullName))
+            {
+                writer.Write(generatedCode);
+                writer.Flush();
+            }
+        }
+    }
+}
--- src/OData.ProxyGenerator.Console/Properties/AssemblyInfo.cs
+++ src/OData.ProxyGenerator.Console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ODataConnectedServiceConsole")]
+[assembly: AssemblyTitle("ConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vagrant")]
-[assembly: AssemblyProduct("ODataConnectedServiceConsole")]
+[assembly: AssemblyProduct("ConsoleApp")]
 [assembly: AssemblyCopyright("Copyright © Vagrant 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a8d0dcc7-505d-4be7-a4bb-da96ba7ea46c")]
+[assembly: Guid("2680af94-b849-4f25-a935-83ac43c8cf2c")]
 
 // Version information for an assembly consists of the following four values:
 //
